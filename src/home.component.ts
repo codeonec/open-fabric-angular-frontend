819e7bd96bef414a56ecbfb34bbcc73adf7f92eb
Ritul Patel <ritul.patel904@gmail.com>
2023-06-06 16:30:23 -0400

    loading state

--- src/home.component.ts
+++ src/home.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from "@angular/common";
 // import { Product } from "./models";
 import { ProductComponent } from "./product.component";
 import { APIServices } from "./api.service";
-import { Observable, map } from "rxjs";
+import { BehaviorSubject, Observable, map } from "rxjs";
 import { RouterLink } from "@angular/router";
 @Component({
   selector: "app-home",
@@ -23,6 +23,17 @@ import { RouterLink } from "@angular/router";
             </a>
           </div>
         </div>
+        <div
+          class="col-12 d-flex-justify-content-center py-5"
+          *ngIf="loading$ | async"
+        >
+          <div
+            class="spinner-border text-secondary mx-auto d-block"
+            role="status"
+          >
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
         <product
           *ngFor="let product of products$ | async"
           [product]="product"
@@ -34,12 +45,17 @@ import { RouterLink } from "@angular/router";
 })
 export class HomeComponent implements OnInit {
   products$!: Observable<any>;
+  loading$ = new BehaviorSubject<boolean>(false);
   // products: Product[] = [];
 
   apiService = inject(APIServices);
   ngOnInit(): void {
-    this.products$ = this.apiService
-      .apiGetAllProducts()
-      .pipe(map((response) => response));
+    this.loading$.next(true);
+    this.products$ = this.apiService.apiGetAllProducts().pipe(
+      map((response) => {
+        this.loading$.next(false);
+        return response;
+      })
+    );
   }
 }
